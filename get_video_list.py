55a8c6cbc682490c9e05f0646d898b4f4ce81b41
LuMeng6 <menglbio112@gmail.com>
2020-01-01 23:45:55 -0500

    Update get_video_list.py and add main.py

--- get_video_list.py
+++ get_video_list.py
@@ -1,6 +1,15 @@
 import requests
 
-def get_video_list(page_number=None):
+# January
+start = 1577808000
+end = 1580486399
+
+page_num = 1
+stop = False
+
+video_list = []
+
+def get_video_list_response(page_number=None):
     url = "http://api.bilibili.com/x/web-interface/newlist?rid=25" # 25 is MMD/3D
     if page_number:
         url += "&pn=%d" % page_number
@@ -12,30 +21,36 @@ def record_video_info(videos, filename=None):
     for video in videos:
         if video["copyright"] == 1: # 1 is original
             if start <= video["pubdate"] <= end:
-                video_list.append(video["aid"])
+                video_info = {video["aid"]: get_video_info(video["stat"])}
+                video_list.append(video_info)
                 end = video["pubdate"]
             elif video["pubdate"] < start:
                 stop = True
 
-# January
-start = 1577808000
-end = 1580486399
+def get_video_info(video_stat):
+    video_info = {
+        "view": video_stat["view"],
+        "like": video_stat["like"],
+        "coin": video_stat["coin"],
+        "favorite": video_stat["favorite"]
+    }
+    return video_info
 
-page_num = 1
-stop = False
+def get_video_list():
+    global page_num, stop
 
-video_list = []
+    f = open("video_list_Jan.txt", "w+")
 
-f = open("video_list_Jan.txt", "w+")
+    # won't stop until hitting the start datetime
+    while not stop:
+        response_json = get_video_list_response(page_num)
+        record_video_info(response_json["data"]["archives"])
+        page_num += 1
 
-# won't stop until hitting the start datetime
-while not stop:
-    response_json = get_video_list(page_num)
-    record_video_info(response_json["data"]["archives"])
-    page_num += 1
+    f.writelines("%s\n" % video for video in video_list)
+    f.writelines("counts: %d" % len(video_list))
+    # f.writelines("end time: %d" % end)
 
-f.writelines("%d\n" % video for video in video_list)
-f.writelines("counts: %d\n" % len(video_list))
-f.writelines("end time: %d\n" % end)
+    f.close()
 
-f.close()
+    return video_list
